6779ecae7fd9ff8a48e999b929a6c673e418d201
elinoza <hillcakmak@gmail.com>
2021-02-19 17:47:52 +0300

    signup page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import { Container, Image, Button } from "react-bootstrap";
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import axios from 'axios'
 import logo from "../logo/Spotify_Logo_Black.png";
 import fb from "../logo/fb.png";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+
 import useForm from "./UseForm"; // IMPORTING THE COMPONENT WITH HOOKS
 
 const SignUp = () => {
-  const { handleChange, values, handleSubmit } = useForm(); // DESTRUCTURING HOOKS TO BE ABLE TO USE THEM IN THIS COMPONENT
+  // const { handleChange, values, handleSubmit } = useForm(); // DESTRUCTURING HOOKS TO BE ABLE TO USE THEM IN THIS COMPONENT
+
+  const [email, setEmail] = useState("test@test.com")
+  const [password, setPassword] = useState("test12345")
+  const history = useHistory()
+
+  const login = async ()=> {
+    const url=process.env.REACT_APP_URL
+    const res = await axios(`${url}users/login}`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: {
+        email, password
+      }, withCredentials: true // use cookies
+    })
+    
+    localStorage.setItem("accessToken", res.data)
+  //  props.history.push("/")
+  }
   return (
     <Container fluid id="signup-page-wrapper">
       <div className="sign-logo-wrapper">
@@ -17,21 +42,23 @@ const SignUp = () => {
           <Image src={fb} id="fb" />
           CONTINUE WITH FACEBOOK
         </Button>
-        <Button className="signup-btn apple my-2">CONTINUE WITH APPLE</Button>
-        <Button className="signup-btn google my-2">CONTINUE WITH GOOGLE</Button>
+        <a href="http://localhost:3005/users/spotifyLoginLogin">  <Button className="signup-btn apple my-2">CONTINUE WITH SPOTIFY</Button></a>
+        <a href="http://localhost:3005/users/googleLogin"> <Button className="signup-btn google my-2">CONTINUE WITH GOOGLE</Button></a>
       </div>
       <h6>OR</h6>
       <div className="form-inputs">
-        <form className="form" onSubmit={handleSubmit}>
-          <label>Email addess or username</label>
+        <form className="form" onSubmit={login}>
+          <label>Email address or username</label>
           <input
             className="form-input"
             id="email" // WITH THIS ID IT CHECKS IF IT'S A VALID EMAIL
             name="email"
             type="email"
             placeholder="Email adress or username"
-            value={values.email} // TAKES THE VALUE FROM MY CUSTOM HOOKS IN USEFORM COMPONENT
-            onChange={handleChange} // THE FUNCTION THAT LISTENS TO THE CHANGE OF THE VALUE
+            value={email} // TAKES THE VALUE FROM MY CUSTOM HOOKS IN USEFORM COMPONENT
+            onChange={e => setEmail(e.target.value)} // THE FUNCTION THAT LISTENS TO THE CHANGE OF THE VALUE
+
+
           />
           <br />
           <label>Password</label>
@@ -41,8 +68,8 @@ const SignUp = () => {
             name="password"
             type="password"
             placeholder="Password"
-            value={values.password}
-            onChange={handleChange}
+            value={password}
+            onChange={e => setPassword(e.target.value)}
           />
           <br />
           <a href="#"> Forgot your password? </a>
